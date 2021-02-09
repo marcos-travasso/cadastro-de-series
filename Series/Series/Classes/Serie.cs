538f3e5934c94d58ee39a25af9c7fbe2d9914db6
Marcos Travasso <marcos.travasso42@gmail.com>
2021-02-09 15:46:33 -0300

    Excluir, atualizar e visualizar série no console

--- Series/Series/Classes/Serie.cs
+++ Series/Series/Classes/Serie.cs
@@ -28,7 +28,8 @@ namespace Series
             retorno += "Gênero: " + this.Genero + Environment.NewLine;
             retorno += "Titulo: " + this.Titulo + Environment.NewLine;
             retorno += "Descrição: " + this.Descricao + Environment.NewLine;
-            retorno += "Ano de Início: " + this.Ano;
+            retorno += "Ano de Início: " + this.Ano + Environment.NewLine;
+            retorno += "Excluido: " + this.Excluido;
             return retorno;
         }
 
